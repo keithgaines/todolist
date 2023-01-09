90142b50c81b6edf78d4d5ce0fc9610cd745bad7
Keith Gaines <patrick.keith.gaines@gmail.com>
2023-01-09 11:21:25 -0600

    changed file heirarchy

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+var items = [];
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+
+    var today = new Date();
+
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    };
+
+    var day = today.toLocaleDateString('en-US', options)
+
+
+    res.render('list', {
+        newListItem: items
+    });
+});
+
+app.post('/', function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/')
+});
+
+app.listen(3000, function() {
+    console.log('server listening on port 3000')
+})
